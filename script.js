93eb14fa34ed351338f38bf25d7dac66cd7bb3e7
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2025-04-22 23:18:39 +0800

    Created operate function
    Handles the operation.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Global variables
+let firstNum = null;
+let secondNum = null;
+let operator = null;
+
 // start by creating functions for all the basic math
 const add = function (a, b) {
   return a + b;
@@ -14,3 +19,21 @@ const multiply = function (a, b) {
 const divide = function (a, b) {
   return a / b;
 };
+
+// console.log(add(2, 2));
+// console.log(subtract(2, 2));
+// console.log(multiply(2, 2));
+// console.log(divide(2, 2));
+
+// Handle Operation
+function operate(op, fNum, sNum) {
+  if (op == "+") {
+    return add(fNum, sNum);
+  } else if (op == "-") {
+    return subtract(fNum, sNum);
+  } else if (op == "*") {
+    return multiply(fNum, sNum);
+  } else if (op == "/") {
+    return divide(fNum, sNum);
+  }
+}
